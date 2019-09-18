b421fc9d017316d912c8ad2afaa5d66cd4becbd9
Graham Voysey <gvoysey@bu.edu>
2019-09-18 14:32:27 -0400

    Ivy C-x C-f, updated build script

--- bin/build-elisp.sh
+++ bin/build-elisp.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+if [ -n "$(pgrep emacsclient)" ]
+   then
+       emacsclient -e '(save-buffers-kill-emacs)'
+       echo "shutting down existing emacs processes before rebuilding"
+fi
 
 emacs --batch -l ./build.el  --eval "(to-elisp \"./user-outlines/$1.org\" t)"
