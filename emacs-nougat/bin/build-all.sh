ec3d9f1cfcce9b88c617edf62e97d1e1a6dea6ec
Graham Voysey <graham@neurala.com>
2020-01-07 11:10:38 -0500

    Remove requirement to kill existing emacs instances before building

--- emacs-nougat/bin/build-all.sh
+++ emacs-nougat/bin/build-all.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-if [ -n "$(pgrep emacsclient)" ]
-   then
-       emacsclient -e '(save-buffers-kill-emacs)'
-       echo "shutting down existing emacs processes before rebuilding"
-fi
+# if [ -n "$(pgrep emacsclient)" ]
+#    then
+#        emacsclient -e '(save-buffers-kill-emacs)'
+#        echo "shutting down existing emacs processes before rebuilding"
+# fi
 
 emacs --batch -l ./build.el  --eval "(to-elisp \"./user-outlines/$1.org\" t)"
 emacs --batch -l ./build.el  --eval "(to-html \"./user-outlines/$1.org\" t)"
