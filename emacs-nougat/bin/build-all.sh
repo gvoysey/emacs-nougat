61478717219b8ea1c6588ca6c452b9407585092d
Graham Voysey <graham@neurala.com>
2020-09-10 22:35:36 -0400

    Updates

--- emacs-nougat/bin/build-all.sh
+++ emacs-nougat/bin/build-all.sh
@@ -4,8 +4,12 @@
 #        emacsclient -e '(save-buffers-kill-emacs)'
 #        echo "shutting down existing emacs processes before rebuilding"
 # fi
-
+echo weaving config for $1
 emacs --batch -l ./build.el  --eval "(to-elisp \"./user-outlines/$1.org\" t)"
 emacs --batch -l ./build.el  --eval "(to-html \"./user-outlines/$1.org\" t)"
+echo regenerating HTML
 rm -f "../docs/index.html"
-ln "./user-outlines/$1.html" "../docs/index.html"
+ln -fn "./user-outlines/$1.html" "../docs/index.html"
+echo setting symlinks
+ln -sfn "./user-outlines/$1.el" "../init.el"
+echo done!
